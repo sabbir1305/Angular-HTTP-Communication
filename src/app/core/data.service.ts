139d47fce208b688b6346cb54af61005aedaac1a
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-04-24 22:19:08 +0600

    CrudOperation for Book

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { allBooks, allReaders } from 'app/data';
@@ -11,7 +11,6 @@ import { BookTrackerError } from 'app/models/bookTrackerError';
   providedIn: 'root'
 })
 export class DataService {
-
   constructor(private http: HttpClient) { }
 
   mostPopularBook: Book = allBooks[0];
@@ -28,11 +27,41 @@ export class DataService {
     return allReaders.find(reader => reader.readerID === id);
   }
 
-  getAllBooks(): Book[] {
-    return allBooks;
+  getAllBooks():Observable<Book[]> {
+    return this.http.get<Book[]>('/api/books');
   }
 
-  getBookById(id: number): Book {
-    return allBooks.find(book => book.bookID === id);
-  }  
+  getBookById(id: number):Observable<Book> {
+    let getHeaders:HttpHeaders = new HttpHeaders({
+      'Accept':'application/json',
+      'Authorization':'my-token'
+    });
+
+    return this.http.get<Book>(`/api/books/${id}`,{
+      headers:getHeaders
+    });
+  }
+
+  addBook(newBook:Book):Observable<Book>{
+
+    return this.http.post<Book>(`/api/books`,newBook,{
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    });
+  }
+
+  updateBook(upBook:Book):Observable<void>{
+    return this.http.put<void>(`/api/books\${upBook.bookID}`,upBook,{
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+
+    });
+  }
+
+  deleeBook(bookID:number):Observable<void>{
+    return this.http.delete<void>(`/api/books/${bookID}`);
+  }
+
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,9 +19,12 @@ export class DashboardComponent implements OnInit {
 
   constructor(private dataService: DataService,
               private title: Title) { }
-  
+
   ngOnInit() {
-    this.allBooks = this.dataService.getAllBooks();
+     this.dataService.getAllBooks().subscribe(
+       (data:Book[])=>this.allBooks=data,
+       (err:any)=>console.log(err)
+     );
     this.allReaders = this.dataService.getAllReaders();
     this.mostPopularBook = this.dataService.mostPopularBook;
 
