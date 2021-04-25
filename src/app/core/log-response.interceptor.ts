844ccf0844d886e58711ff16c761a073abb311e7
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-04-25 23:57:12 +0600

    Interceptor

---
+++ src/app/core/log-response.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+@Injectable()
+
+export class LogResponseInterceptor implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(`Log Response Interceptor- ${req.url}`);
+
+    return next.handle(req).pipe(
+      tap(event=>{
+        if(event.type==HttpEventType.Response){
+          console.log(event.body);
+
+        }
+      })
+    )
+
+  }
+
+}
